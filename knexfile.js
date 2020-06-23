154d21e634fce13d83ffd697380141d386f0d69b
jefftcraig <jeffcraig35@gmail.com>
2020-06-23 19:51:57 -0400

    Try using db url only

--- knexfile.js
+++ knexfile.js
@@ -26,14 +26,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: {
-      url: process.env.DATABASE_URL,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
