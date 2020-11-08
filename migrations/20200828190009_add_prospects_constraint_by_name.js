2b9e6a29784af28a65d23aaccb0857a7ab2f6845
jefftcraig <jeffcraig35@gmail.com>
2020-11-07 19:46:59 -0500

    Remove not null for game_statline field, make dob part of constraint and not draft year

--- migrations/20200828190009_add_prospects_constraint_by_name.js
+++ migrations/20200828190009_add_prospects_constraint_by_name.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
   return knex.schema.alterTable('prospects', function(t) {
-    t.unique(['last_name', 'first_name', 'draft_year'])
+    t.unique(['last_name', 'first_name', 'dob'])
   })
 };
 
 exports.down = function(knex) {
   knex.schema.table('prospects', function (table) {
-    table.dropUnique(['last_name', 'first_name', 'draft_year'])
+    table.dropUnique(['last_name', 'first_name', 'dob'])
   })
 };
