9c65c34125b46e912e9d728b89a4c4eb9fd05b21
jefftcraig <jeffcraig35@gmail.com>
2021-03-14 14:05:25 -0400

    Add ssl to production

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,10 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      database: process.env.DATABASE_URL,
+      ssl: true,
+    },
     pool: {
       min: 2,
       max: 10,
